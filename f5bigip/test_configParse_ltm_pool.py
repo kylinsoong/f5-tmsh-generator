c580afd41cab13a166b6c94fc4693d4d6b443cd2
kylin <kylinsoong.1214@gmail.com>
2023-11-08 14:00:09 +0800

    #25 add reproduce config and unit test

---
+++ f5bigip/test_configParse_ltm_pool.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import unittest
+import os
+import time
+
+from configParse import *
+
+def load_config_data(filename):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    parent_folder = os.path.dirname(current_directory)
+    file_path = os.path.join(parent_folder, "config", filename)
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+            file.close()
+            content = content.replace('[m','')
+            content = content.replace('[K', '')
+            error = re.findall(r'\[7m---\(less (\d+)', content)
+            for i in error:
+                error1 = '[7m---(less '+i
+                content = content.replace(error1, '')
+            return content
+    else:
+        return None
+
+
+class TestConfigParseValidation(unittest.TestCase):
+
+    def test_data_search(self):
+        data = load_config_data("issue-25.txt")
+        if data is not None: 
+            pool_list = ltm_pool(data)
+            self.assertEqual(len(net_route_list), 2)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
